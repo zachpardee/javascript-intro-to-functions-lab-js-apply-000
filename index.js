1cbb27c4076f65b779139e6d02ee4754032eea20
awesome-edition-6075 <zachpardee@gmail.com>
2019-01-08 22:15:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,4 +19,12 @@ function sayHiToGrandma(string){
   {
     return "I can't hear you!";
   }
+    if (string === string.toUpperCase())
+  {
+    return "YES INDEED!";
+  }
+    if (string === "I love you, Grandma")
+  {
+    return "I love you too.";
+  }
 }
