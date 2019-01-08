8a13ecea044c9f4e3a6b3874493e43d7616d1a66
awesome-edition-6075 <zachpardee@gmail.com>
2019-01-08 22:08:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,7 @@ function whisper(string) {
 function logShout(string) {
   return console.log(string.toUpperCase());
 }
+
+function logWhisper(string) {
+  return console.log(string.toLowerCase());
+}
